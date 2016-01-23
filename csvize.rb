d19e15241ce60d183f5e9d81589461fe1055f498
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-23 15:46:03 +0100

    Convert to CSV

--- csvize.rb
+++ csvize.rb
@@ -10,4 +10,11 @@ database = JSON::load(open(ARGV[0]))
 output = ARGV[1]
 
 [('A'...'Z').to_a, 'Å', 'Ä', 'Ö'].flatten.each do |letter|
+  proverbs = database[letter]
+  next if proverbs.nil?
+  CSV.open(File.join(output, "#{letter}.csv"), 'wb') do |file|
+    proverbs.map do |proverb|
+      file << [proverb['expression'], proverb['translation'], proverb['meaning']]
+    end
+  end
 end
