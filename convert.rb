0b0aefe9cb3238d3ef3cd77d003eed4dae40ed7b
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-23 13:49:04 +0100

    Implement basic parsing

--- convert.rb
+++ convert.rb
@@ -5,3 +5,19 @@ require 'nokogiri'
 raise 'an HTML file is required' if ARGV.length < 1
 
 page = Nokogiri::HTML(open(ARGV[0]))
+elements = page.css('#mw-content-text')[0].children
+
+letter = nil
+elements.each do |element|
+  case element.node_name
+  when 'h2'
+    letter = element.css('.mw-headline').text
+    next unless letter.length == 1
+    puts "Letter: #{letter}"
+  when 'ul'
+    text = element.css('i')[0]
+    next if text.nil?
+    text = text.text
+    puts "Pronoun: #{text}"
+  end
+end
